89ed59eee8d53650cb3bda441c819cdeddea19a9
Manuel Vasquez Cruz <manu.kurusu@gmail.com>
2024-04-29 15:03:41 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.0-RC1"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
